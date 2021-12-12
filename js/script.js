40e2b3aa13f3858e84e80f13a1615941b98df236
satasramunas <ramunas.satas@outlook.com>
2021-12-12 15:13:34 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function doSomething() {
+
+    let elem = document.getElementById("my-div");
+    elem.style.border = "1px solid brown";
+    elem.style.padding = "10px";
+    elem.style.margin = "10px";
+}
